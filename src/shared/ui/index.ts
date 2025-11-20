2359ad6222bb684ce1ba2969dcaaec53cd567496
kiki-do <dan.antipov.kiki@gmail.com>
2025-11-20 22:53:28 +0300

    🔧 chore(modal): add modal component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,7 @@
 export * from './badge';
 export * from './button';
 export * from './item';
+export * from './modal';
 export * from './separator';
 export * from './skeleton';
 export * from './tooltip';
